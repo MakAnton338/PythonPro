085b98c06299b52d7010f307a8a980a5ca77540e
MakAnton <Makanton95@gmail.com>
2023-02-14 21:17:14 +0200

    HW_1

--- main.py
+++ main.py
@@ -22,6 +22,17 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&?color=purple') == {'name': 'ferret', '?color': 'purple'}
     assert parse('?name=ferret&color=purple?') == {'name': 'ferret', 'color': 'purple?'}
 
+def parse_cookie(query: str) -> dict:
+    return {}
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+
+
 
 
 
