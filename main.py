3a9040420773973bec51322c3df7eccdf51759f0
MakAnton <Makanton95@gmail.com>
2023-02-14 21:32:56 +0200

    just_main

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from urllib.parse import parse_qsl, urlparse
-
 def parse(query: str) -> dict:
     return {}
 
@@ -13,35 +11,14 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    query_split = query.split(";")
-    if len(query_split) <= 1:
-        return query
-    query_dict = {}
-    for i in query_split:
-        split_item = i.strip()
-        split_item = split_item.split('=', 1)
-        if len(split_item) == 2:
-            k, v = split_item
-            query_dict[k] = v
-    print(query_dict)
-    return query_dict
+    return {}
+
 
 if __name__ == '__main__':
-    assert parse_cookie('') == ('')
-    assert parse_cookie('=') == ('=')
-    assert parse_cookie('name=John;') == {'name': 'John'}
-    assert parse_cookie('name=John;age=28;') == {'name': 'John', 'age': '28'}
-    assert parse_cookie('name=John=User;age=28;') == {'name': 'John=User', 'age': '28'}
-    assert parse_cookie('name=John; city=Dnipro?') == {'name': 'John', 'city': 'Dnipro?'}
-    assert parse_cookie('name=John;age=?8;') == {'name': 'John', 'age': '?8'}
-    assert parse_cookie('name!=John=User;age=28;') == {'name!': 'John=User', 'age': '28'}
-    assert parse_cookie('name=John;age= ') == {'name': 'John', 'age': ''}
-    assert parse_cookie('1213=;') == {'1213': ''}
-    assert parse_cookie('name=John=age=28;') == {'name': 'John=age=28'}
-    assert parse_cookie('name=; John=User; age=28;') == {'name': '', 'John': 'User', 'age': '28'}
-    assert parse_cookie('=; name=John;') == {'': '', 'name': 'John'}
-
-
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
 
 
 
